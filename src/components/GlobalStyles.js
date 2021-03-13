b809e05986c75696b3743174b67fed339ee9cf8f
vicky <vicky.7dev@gmail.com>
2021-03-13 18:47:27 +0530

    project setup

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    body{
+        padding:  0;
+        margin: 0;
+        box-sizing: border-box;
+        font-size : 10px;
+        font-family: 'Montserrat', sans-serif;    
+    }
+
+`;
+
+export default GlobalStyles;
